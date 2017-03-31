0ad7b2b4fce310931a39b5bc4fcef9edde16cfa6
diegofsousa <diegofernando5672@gmail.com>
2017-03-31 12:57:36 -0300

    alter4

--- SudColor/settings.py
+++ SudColor/settings.py
@@ -151,3 +151,12 @@ STATIC_URL = '/static/'
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'core/static/'),
 )
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+    }
+}
+
+DEBUG = True
