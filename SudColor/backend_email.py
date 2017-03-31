8faa04ab929522bbbb1b218a53233108b0b6b9f4
diegofsousa <diegofernando5672@gmail.com>
2017-03-31 13:35:14 -0300

    remove -----

--- SudColor/backend_email.py
+++
@@ -1,6 +0,0 @@
-#Servidor de email do Gmail
-EMAIL_USE_TLS = True
-EMAIL_HOST = 'smtp.gmail.com'
-EMAIL_HOST_USER = 'backendsudestecolor@gmail.com'
-EMAIL_HOST_PASSWORD = 'oieusouobahia454565'
-EMAIL_PORT = 587
